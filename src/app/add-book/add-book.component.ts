434663368b7877f47beba3583411a0d47af81ceb
Devarsh shukla <Devarsh14@gmail.com>
2020-04-10 11:28:56 +1000

    10Apritl start.

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { CreateBookService } from '../create-book.service';
+
 
 @Component({
   selector: 'app-add-book',
@@ -10,14 +12,28 @@ import { FormBuilder } from '@angular/forms';
 export class AddBookComponent implements OnInit {
 
   addBookToLibraryForm = this.fb.group({
-  bookname: ['', [Validators.minLength(3), Validators.required]],
-  bookISBN: ['', Validators.required],
+    BooksName: ['', [Validators.minLength(3), Validators.required]],
+    AuthorName: ['', Validators.required],
+    // Isbn: ['', Validators.required],
+    // BookPrice: ['', Validators.required],
 });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private createBookService: CreateBookService ) { }
 
   get bookname() {
-    return this.addBookToLibraryForm.get('bookname');
+    return this.addBookToLibraryForm.get('BooksName');
+  }
+
+  onSubmit() {
+    console.warn(this.addBookToLibraryForm.value);
+    this.createBookService.create(this.addBookToLibraryForm.value)
+    .subscribe(
+      data => console.log('Success data', data),
+      error => console.error('Error', error)
+    );
+
+
+
   }
   ngOnInit(): void {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { CreateBookService } from './create-book.service';
+import { ListOfBooksComponent } from './list-of-books/list-of-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddBookComponent
+    AddBookComponent,
+    ListOfBooksComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CreateBookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
